1ad9c81956658ad732a6e1b6abf984a296367c5c
OuTrabelsi <oussematrappa@gmail.com>
2025-02-15 17:42:23 +0100

    My Commit - Rendez Vous & Logement

--- LogementRendezVous-Etudiant/src/main/java/webservices/ApplicationMain.java
+++ LogementRendezVous-Etudiant/src/main/java/webservices/ApplicationMain.java
@@ -2,7 +2,11 @@ package webservices;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
-@ApplicationPath("api")
+@ApplicationPath("/api")
 public class ApplicationMain extends Application {
-}
+
+    }
+
